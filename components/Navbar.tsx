c7d558a32e1f2b127ea625910bfd7e891ed32d14
Viktor Kopan <victorkopan98@gmail.com>
2023-10-05 18:04:55 +0600

    created Billboard component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   return (
     <nav className="fixed z-40 w-full">
       <div
-        className={`flex flex-row items-center bg-zinc-900 bg-opacity-90 px-4 py-6 transition duration-500 md:px-16 ${
+        className={`flex flex-row items-center px-4 py-6 transition duration-500 md:px-16 ${
           showBackgroundMenu ? "bg-zinc-900 bg-opacity-90" : ""
         }`}
       >
